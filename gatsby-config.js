e5b2e0b537a13e1e535b21d1a689abd1394d80e0
Leigh Halliday <leighhalliday@gmail.com>
2018-10-27 21:53:32 -0400

    Updating contentful key

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: 'Heather Halliday',
+    title: "Heather K. Halliday"
   },
   plugins: [
     `gatsby-plugin-typescript`,
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-116316556-1',
+        trackingId: "UA-116316556-1",
         head: false,
         anonymize: true,
-        respectDNT: true,
-      },
+        respectDNT: true
+      }
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: '16h24zcri5xr',
+        spaceId: "les2y5injkn5",
         accessToken:
-          '80c172a3d3f27b654fa417212c8b9c2a54f528950a958d948bab832ce805334b',
-      },
+          "23603194dd62b940037cccc12b2b379a400431b0bdf20497c0945ffa7a069fa3"
+      }
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-  ],
+    `gatsby-plugin-netlify`
+  ]
 }
